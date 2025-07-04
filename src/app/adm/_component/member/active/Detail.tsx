1c5d9cd0bdf07b6745c4140a0f7d74a56b9a3737
jinhye-pnh <jinhye@pnhglobal.co.kr>
2025-07-04 11:27:23 +0900

    Refactor: input 컴포넌트로 교체

--- src/app/adm/_component/member/active/Detail.tsx
+++ src/app/adm/_component/member/active/Detail.tsx
@@ -12,11 +12,30 @@ import CommonModal from '@/adm/_component/common/modals/CommonModal';
 import Editor from '@/adm/_component/common/inputs/Editor';
 import Button from '@/adm/_component/common/buttons/Button'
 import {ButtonProps} from '@/types/components'
+import LabelInput from '@/adm/_component/common/inputs/LabelInput';
+import LabelInputSet from '@/adm/_component/common/inputs/LabelInputSet';
+import LabelTextarea from '@/adm/_component/common/inputs/LabelTextarea';
 
 
 type PageProps = {
   id: string;
 };
+
+interface JsonData {
+  id: string | number;
+  content: string;
+  createdAt: string | Date;
+  updatedAt: string | Date;
+  userName: string;
+  email: string;
+  address: string;
+  currentPswd: string;
+  newPswd: string;
+  confirmPswd: string;
+  data1: string;
+  data2: string;
+  data3: string;
+}
 
 export default function Page({ id }: PageProps) {
   const router = useRouter();
@@ -28,11 +47,22 @@ export default function Page({ id }: PageProps) {
     gcTime: 300 * 1000, // 5분뒤 메모리 정리
     enabled: !!id,
   });
-  const [jsonData, setJsonData] = useState({
+  const [jsonData, setJsonData] = useState<JsonData>({
     id: data?.id || 0,
     content: data?.content || '',
+    createdAt: data?.created_at || '',
+    updatedAt: data?.updated_at || '',
+    userName: "",
+    email: "user@example.com",
+    address: "",
+    currentPswd: "",
+    newPswd: "",
+    confirmPswd: "",
+    data1: "",
+    data2: "",
+    data3: "",
   });
-  const {handleChange, handleCustomChange} = onInputsChange(jsonData, setJsonData);
+  const {handleChange} = onInputsChange(jsonData, setJsonData);
   const confirmationButtons: ButtonProps[] = [
     {
       text: '정지',
@@ -57,81 +87,82 @@ export default function Page({ id }: PageProps) {
     console.log("클릭했습니다.")
   }
 
-  const updateMutation = useMutation<ApiResponse<Board>, Error>({
-    mutationFn: async () => {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/protected/board/${data?.id}`, {
-        method: 'PATCH',
-        cache: 'no-store',
-        headers: {
-          'Content-Type': 'application/json',
-          'Cache-Control': 'no-store',
-        },
-        body: JSON.stringify(jsonData),
-      });
+  // const updateMutation = useMutation<ApiResponse<Board>, Error>({
+  //   mutationFn: async () => {
+  //     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/protected/board/${data?.id}`, {
+  //       method: 'PATCH',
+  //       cache: 'no-store',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         'Cache-Control': 'no-store',
+  //       },
+  //       body: JSON.stringify(jsonData),
+  //     });
+  //
+  //     if (!response.ok) {
+  //       throw new Error("통신 오류 발생!");
+  //     }
+  //
+  //     return response.json();
+  //   },
+  //   async onSuccess(res) {
+  //     // boardList 갱신
+  //     queryClient.setQueryData(['boardList'], (prevData?: Board[]) => {
+  //       if (!prevData) return [];
+  //
+  //       const newList = prevData.map((item) =>
+  //         item.id === res.data.id ? { ...res.data, rn: prevData.indexOf(item) + 1 } : item
+  //       );
+  //
+  //       return newList;
+  //     });
+  //
+  //     // board 갱신
+  //     queryClient.setQueryData(['board', id], (prevData?: board) => {
+  //       return res.data;
+  //     });
+  //
+  //     router.back();
+  //   },
+  //   onError() {
+  //     alert('오류가 발생하였습니다. 관리자에게 문의하세요.');
+  //   },
+  // });
 
-      if (!response.ok) {
-        throw new Error("통신 오류 발생!");
-      }
 
-      return response.json();
-    },
-    async onSuccess(res) {
-      // boardList 갱신
-      queryClient.setQueryData(['boardList'], (prevData?: Board[]) => {
-        if (!prevData) return [];
-
-        const newList = prevData.map((item) =>
-          item.id === res.data.id ? { ...res.data, rn: prevData.indexOf(item) + 1 } : item
-        );
-
-        return newList;
-      });
-
-      // board 갱신
-      queryClient.setQueryData(['board', id], (prevData?: board) => {
-        return res.data;
-      });
-
-      router.back();
-    },
-    onError() {
-      alert('오류가 발생하였습니다. 관리자에게 문의하세요.');
-    },
-  });
-
-
-  const deleteMutation = useMutation<ApiResponse<Board>, Error>({
-    mutationFn: async () => {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/protected/board/${data?.id}`, {
-        method: 'DELETE',
-        cache: 'no-store',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(jsonData),
-      });
-
-      return response.json();
-    },
-    async onSuccess() {
-      queryClient.setQueryData(['boardList'], (prevData?: Board[]) => {
-        if (!prevData) return [];
-        let newList = [...prevData];
-        return newList.filter((item) => item.id !== jsonData.id);
-      });
-
-      router.back();
-    },
-    onError() {
-      alert('오류가 발생하였습니다. 관리자에게 문의하세요.');
-    },
-  });
+  // const deleteMutation = useMutation<ApiResponse<Board>, Error>({
+  //   mutationFn: async () => {
+  //     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/protected/board/${data?.id}`, {
+  //       method: 'DELETE',
+  //       cache: 'no-store',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify(jsonData),
+  //     });
+  //
+  //     return response.json();
+  //   },
+  //   async onSuccess() {
+  //     queryClient.setQueryData(['boardList'], (prevData?: Board[]) => {
+  //       if (!prevData) return [];
+  //       let newList = [...prevData];
+  //       return newList.filter((item) => item.id !== jsonData.id);
+  //     });
+  //
+  //     router.back();
+  //   },
+  //   onError() {
+  //     alert('오류가 발생하였습니다. 관리자에게 문의하세요.');
+  //   },
+  // });
 
   useEffect(() => {
     if (data) {
-      setJsonData({
-        ...data,
-      });
+      setJsonData((prevState)=>({
+        ...prevState,
+        data,
+      }));
     }
   }, [data]);
 
@@ -148,43 +179,84 @@ export default function Page({ id }: PageProps) {
               <div className={styles.avatarSkeleton}></div>
             </div>
             <div className={styles.inner_column}>
-              <div className={styles.inner_row}>
-                <label className={styles.label}>이름</label>
-                <input type="text" placeholder="이름" className={styles.input} />
-              </div>
-              <div className={styles.inner_row}>
-                <label className={styles.label}>이메일</label>
-                <input type="email" value="user@example.com" disabled className={styles.inputDisabled} />
-              </div>
+              <LabelInput
+                label="이름"
+                name="userName"
+                value={jsonData.userName}
+                maxLength={10}
+                placeholder="이름"
+                disabled
+                onChange={handleChange}
+              />
+              <LabelInput
+                label="이메일"
+                name="email"
+                value={jsonData.email}
+                maxLength={80}
+                placeholder="이메일"
+                disabled
+                onChange={handleChange}
+              />
             </div>
           </li>
 
+          {/* todo 비밀번호 변경은 모달로 빼기 */}
           <li className={styles.one_row_box}>
-            <div className={styles.inner_row}>
-              <label className={styles.label}>비밀번호 변경</label>
-              <div className={styles.inner_row_set}>
-                <input type="password" placeholder="새 비밀번호" className={styles.input} />
-                <input type="password" placeholder="비밀번호 확인" className={styles.input} />
-                <Button
-                  text="변경"
-                  variant="contained"
-                  color="primary"
-                  size="md"
-                  height="100%"
-                  onClick={()=>console.log("확인")}
-                />
-              </div>
-            </div>
+            <LabelInputSet label="비밀번호 변경">
+              <LabelInput
+                type="newPswd"
+                name="newPswd"
+                placeholder="새 비밀번호"
+                value={jsonData.newPswd}
+                maxLength={15}
+                showCharCount
+                showLabel={false}
+                onChange={handleChange}
+              />
+              <LabelInput
+                type="confirmPswd"
+                name="confirmPswd"
+                placeholder="비밀번호 확인"
+                value={jsonData.confirmPswd}
+                maxLength={15}
+                showCharCount
+                showLabel={false}
+                onChange={handleChange}
+              />
+              <Button
+                text="변경"
+                variant="contained"
+                color="primary"
+                size="md"
+                height="100%"
+                onClick={()=>console.log("확인")}
+              />
+            </LabelInputSet>
           </li>
 
           <li className={styles.input_box}>
-            <label className={styles.label}>주소</label>
-            <input type="text" placeholder="주소" className={styles.input} />
+            <LabelInput
+              label="주소"
+              name="address"
+              value={jsonData.address}
+              maxLength={50}
+              placeholder="주소를 입력하세요"
+              showCharCount
+              required
+              onChange={handleChange}
+            />
           </li>
 
           <li className={styles.input_box}>
-            <label className={styles.label}>특이사항 (관리자 메모)</label>
-            <textarea placeholder="특이사항" rows={4} className={styles.textarea} />
+            <LabelTextarea
+              label="회원정보3"
+              name="data3"
+              value={jsonData.data3}
+              maxLength={3000}
+              placeholder="회원정보3을 입력하세요"
+              showCharCount
+              onChange={handleChange}
+            />
           </li>
         </ul>
     </CommonModal>
---
+++ src/app/adm/member/active/[id]/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Detail from '@/adm/_component/member/active/Detail';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import { getBoard } from '@/services/boardService';
+import ReactQueryProviders from '@/providers/ReactQueryProvider';
+
+type PageParams = { params: { id: string } };
+
+export default async function Page({ params }: PageParams) {
+  const { id } = await params;
+
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({ queryKey: ['board'], queryFn: () => getBoard(id) });
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <ReactQueryProviders>
+      <HydrationBoundary state={dehydratedState}>
+        <Detail id={id} />
+      </HydrationBoundary>
+    </ReactQueryProviders>
+  );
+}
